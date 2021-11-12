62e8c91565115de569fa89d991ac9c759b5d850b
Wole Ajewole <wolemercy@gmail.com>
2021-11-12 09:12:34 +0100

    Update database settings

--- developsnews/celery.py
+++ developsnews/celery.py
@@ -6,7 +6,7 @@ from celery.schedules import crontab
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'developsnews.settings')
 
 app = Celery('developsnews')
-# app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.conf.beat_schedule = {
     'refresh_votes': {
--- developsnews/settings.py
+++ developsnews/settings.py
@@ -120,10 +120,9 @@ if SYSTEM_ENV != 'PRODUCTION':
         }
 
 else:
-    DATABASES = {}
-    db_from_env = dj_database_url.config(conn_max_age=600)
-    DATABASES['default'].update(db_from_env)
-
+    DATABASES = {
+        'default': dj_database_url.config()
+        }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
